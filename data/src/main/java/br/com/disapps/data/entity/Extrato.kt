ce40cb715dff7d05cdb97691c5a6ae353ac60e7f
Diefferson Santos <diefferson.sts@gmail.com>
2018-05-27 21:44:35 -0300

    serialied names

--- data/src/main/java/br/com/disapps/data/entity/Extrato.kt
+++ data/src/main/java/br/com/disapps/data/entity/Extrato.kt
@@ -1,11 +1,17 @@
 package br.com.disapps.data.entity
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * Created by diefferson.santos on 10/05/17.
  */
 class Extrato {
+    @SerializedName("saldo")
     var saldo: Double = 0.0
+    @SerializedName("local")
     var local: String = ""
+    @SerializedName("data")
     var data: String = ""
+    @SerializedName("valor")
     var valor: Double = 0.0
 }
