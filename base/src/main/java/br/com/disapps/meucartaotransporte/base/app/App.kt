62e617f9a6472d4e2809bb43ba18a430967c53a3
Diefferson Santos <diefferson.sts@gmail.com>
2018-06-21 00:28:50 -0300

    base featrue

---
+++ base/src/main/java/br/com/disapps/meucartaotransporte/base/app/App.kt
@@ -0,0 +1,34 @@
+package br.com.disapps.meucartaotransporte.base.app
+
+import android.support.multidex.MultiDexApplication
+import android.support.v7.app.AppCompatDelegate
+import br.com.disapps.data.storage.database.Database
+import org.koin.android.ext.android.inject
+import org.koin.android.ext.android.startKoin
+import org.koin.log.EmptyLogger
+
+/**
+ * Created by dnso on 08/03/2018.
+ */
+
+class App : MultiDexApplication() {
+
+    private val database : Database by inject()
+
+    override fun onCreate() {
+        super.onCreate()
+
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+
+        instance = this
+
+        startKoin(this, AppInject.modules(), logger = EmptyLogger())
+
+        database.initDatabase()
+    }
+
+    companion object {
+        var instance: App? = null
+            private set
+    }
+}
