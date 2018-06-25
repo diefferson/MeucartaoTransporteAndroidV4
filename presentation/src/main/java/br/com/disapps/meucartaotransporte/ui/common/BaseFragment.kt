e9e06470b8f8de62cdecb2a037d6357294bc9d3c
diefferson.santos <diefferson.santos@gft.com>
2018-06-25 13:42:22 -0300

    remove databinding base classes

--- presentation/src/main/java/br/com/disapps/meucartaotransporte/ui/common/BaseFragment.kt
+++ presentation/src/main/java/br/com/disapps/meucartaotransporte/ui/common/BaseFragment.kt
@@ -1,15 +1,12 @@
 package br.com.disapps.meucartaotransporte.ui.common
 
 import android.arch.lifecycle.Observer
-import android.databinding.DataBindingUtil
-import android.databinding.ViewDataBinding
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import br.com.disapps.meucartaotransporte.BR
 import br.com.disapps.meucartaotransporte.R
 import com.appodeal.ads.Appodeal
 
@@ -21,7 +18,6 @@ abstract class BaseFragment : Fragment() {
     abstract val viewModel: BaseViewModel
     abstract val fragmentLayout: Int
     abstract val fragmentTag:String
-    private var binding: ViewDataBinding? = null
 
     var hasTabs: Boolean = false
 
@@ -30,7 +26,7 @@ abstract class BaseFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?{
-        val view = initDataBinding(inflater,container)
+        val view = inflater.inflate(fragmentLayout,container, false)
         setupLoading()
         setupError()
         return view
@@ -39,13 +35,6 @@ abstract class BaseFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         Appodeal.cache(activity!!, Appodeal.NATIVE)
-    }
-
-    private fun initDataBinding(inflater: LayoutInflater, container: ViewGroup?) : View?{
-        binding = DataBindingUtil.inflate(inflater, fragmentLayout, container, false)
-        binding?.setVariable(BR.viewModel, viewModel)
-        binding?.setLifecycleOwner(this)
-        return binding?.root
     }
 
     open fun setupLoading(){
