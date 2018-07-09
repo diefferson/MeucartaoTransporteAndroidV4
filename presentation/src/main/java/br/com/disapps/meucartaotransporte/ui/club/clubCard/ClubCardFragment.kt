8bb9fd0f65eadfc9eba463cb6ff1879cb5181d43
diefferson.santos <diefferson.santos@gft.com>
2018-07-09 18:42:53 -0300

    qr code

--- presentation/src/main/java/br/com/disapps/meucartaotransporte/ui/club/clubCard/ClubCardFragment.kt
+++ presentation/src/main/java/br/com/disapps/meucartaotransporte/ui/club/clubCard/ClubCardFragment.kt
@@ -1,18 +1,37 @@
 package br.com.disapps.meucartaotransporte.ui.club.clubCard
 
+import android.os.Bundle
+import android.view.View
 import br.com.disapps.meucartaotransporte.R
 import br.com.disapps.meucartaotransporte.ui.common.BaseFragment
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
+import com.journeyapps.barcodescanner.BarcodeEncoder
+import kotlinx.android.synthetic.main.fragment_club_card.*
 import org.koin.android.architecture.ext.viewModel
 
 class ClubCardFragment :BaseFragment(){
 
     override val viewModel: ClubCardViewModel by viewModel()
-
-    override val fragmentLayout = R.layout.fragment_recycler
-
+    override val fragmentLayout = R.layout.fragment_club_card
     override val fragmentTag = "ClubCardFragment"
 
     companion object {
         fun newInstance() = ClubCardFragment()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val text="123456789"
+        val multiFormatWriter =  MultiFormatWriter()
+        try {
+            val bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,200,200)
+            val barcodeEncoder = BarcodeEncoder()
+            val bitmap = barcodeEncoder.createBitmap(bitMatrix)
+            qr_code.setImageBitmap(bitmap)
+        } catch ( e: WriterException) {
+            e.printStackTrace()
+        }
     }
 }
