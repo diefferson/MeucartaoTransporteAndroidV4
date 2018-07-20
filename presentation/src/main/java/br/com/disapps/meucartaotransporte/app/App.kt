5c021e294d250b9e795bed35582c0679d6518c06
diefferson.santos <diefferson.santos@gft.com>
2018-07-20 11:42:00 -0300

    One signal

--- presentation/src/main/java/br/com/disapps/meucartaotransporte/app/App.kt
+++ presentation/src/main/java/br/com/disapps/meucartaotransporte/app/App.kt
@@ -4,6 +4,7 @@ import android.support.multidex.MultiDexApplication
 import android.support.v7.app.AppCompatDelegate
 import br.com.disapps.data.storage.database.Database
 import br.com.disapps.data.storage.preferences.Preferences
+import com.onesignal.OneSignal
 import org.koin.android.ext.android.inject
 import org.koin.android.ext.android.startKoin
 import org.koin.log.EmptyLogger
@@ -27,6 +28,11 @@ class App : MultiDexApplication() {
         startKoin(this, AppInject.modules(), logger = EmptyLogger())
 
         database.initDatabase()
+
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .init()
+
     }
 
     companion object {
